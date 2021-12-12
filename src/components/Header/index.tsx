74b18d549a84ff6a7e92f99c454c287f2932270b
Mateus <mthenriqueo@gmail.com>
2021-12-12 02:05:09 -0300

    componentização da aplicação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,14 @@
+import logoImg from "../../assets/logo.svg";
+import { Container, Content } from "./styles";
+
 export function Header() {
     return (
-        <header>
-            Header
-        </header>
+        <Container>
+            <Content>
+                <img src={ logoImg } alt="dt money" />
+
+                <button type="button">Nova transação</button>
+            </Content>
+        </Container>
     )
 }
